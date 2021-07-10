19bd82d4eb77d7e93e80e34e0f6e82ebf9d1816c
astar <a.starynskyi@yahoo.com>
2021-07-10 18:09:44 -0500

    few more tasks added

--- src/Algorithms/RenderTemplate.java
+++ src/Algorithms/RenderTemplate.java
@@ -6,10 +6,11 @@ import java.util.Map;
 public class RenderTemplate {
     public static void main(String[] args) {
         //    create solution which will parse the string
-        String str = "Hi, I am {{firstName}} {{lastName}}";
+        String str = "Hi, I am {{firstName}} {{lastName}} and I line in {{city}}.";
         Map<String, String> map = new HashMap<>();
         map.put("firstName", "Ara");
         map.put("lastName", "Livingstone");
+        map.put("city", "Toronto");
         System.out.println(stringParser(str, map));
     }
 
@@ -17,7 +18,7 @@ public class RenderTemplate {
         String[] arr = str.split(" ");
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].contains("{{")) {
-               String tempStr = arr[i].replace("{{", "").replace("}}", "");
+               String tempStr = arr[i].replaceAll("[^a-zA-Z]","");
                 if (map.containsKey(tempStr)) {
                     arr[i] = map.get(tempStr);
                 }
