a1f67407d919dcc80ee73504b0d44cb98ef67011
astarynskyi <a.starynskyi@yahoo.com>
2021-10-20 22:48:53 -0500

    unique numbers counter method updated

--- src/Algorithms/UniqueNumbersCounter.java
+++ src/Algorithms/UniqueNumbersCounter.java
@@ -1,6 +1,8 @@
 package Algorithms;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UniqueNumbersCounter {
     public static void main(String[] args) {
@@ -10,6 +12,19 @@ public class UniqueNumbersCounter {
 
     public static Integer countUniqueNumbers(List<Integer> list) {
         Integer counter = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer i : list) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 0) {
+                counter++;
+            }
+        }
         return counter;
     }
 }
