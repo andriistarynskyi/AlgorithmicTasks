726220172c5b95d96203e6768288bdaa90070766
astarynskyi <a.starynskyi@yahoo.com>
2021-10-20 22:44:33 -0500

    counter unique numbers task added

---
+++ src/Algorithms/UniqueNumbersCounter.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+import java.util.List;
+
+public class UniqueNumbersCounter {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 3, 3, 3);
+        System.out.println(countUniqueNumbers(list));
+    }
+
+    public static Integer countUniqueNumbers(List<Integer> list) {
+        Integer counter = 0;
+        return counter;
+    }
+}
